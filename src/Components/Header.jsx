e76203fd4cf1de8d452fc65736db9468cd9d42bb
John  Busayo <116476931+JohnBusayo14@users.noreply.github.com>
2023-09-13 06:09:20 -0700

    pass
    pass

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Header.css";
+import { Link } from 'react-router-dom';
+
+
+
+const Header = () => {
+  return (
+    <header className='header'>
+     
+      <div className='header-content flex items-center justify-center flex-col text-center'>
+      
+        <h1 className='text-white text-4xl font-bold header-title ls-2'>Welcome to our Course Listing Website</h1>
+        <p className='text-uppercase font-medium text-lg text-white my-3 ls-1'>Learn Without Limit</p>
+        <div className=' w-[50%] flex m-auto gap-5'>
+      <button className=' w-24 h-[40px] bg-white rounded-md font-semibold hover:bg-[#4c956c] hover:text-white'>About Us</button>
+     <Link to="/team">
+     <button className=' w-40 h-[40px] bg-white rounded-md font-semibold hover:bg-[#4c956c] hover:text-white'>Meet our Developer</button>
+     </Link>
+      </div>
+      </div>
+      
+    </header>
+  )
+}
+
+export default Header
